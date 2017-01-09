f4ba84b5ab2370096d33abc8d2e370bd9d5f68f4
Jahmaul <test@youremail.com>
2017-01-09 13:04:59 -0500

    Add #noqa to lengthy url links

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,17 +3,17 @@ import media
 
 empire_strikes_back = media.Movie(
         "Star Wars",
-        "https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg",
+        "https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg", # noqa
         "https://youtu.be/xESiohGGP7g")
 
 the_godfather = media.Movie(
         "The_GodFather",
-        "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",
+        "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg", #noqa
         "https://youtu.be/YYKxj8qiLTg")
 
 the_five_heartbeats = media.Movie(
         "The Five Heartbeats",
-        "https://upload.wikimedia.org/wikipedia/en/7/7f/The_Five_Heartbeats.jpg",
+        "https://upload.wikimedia.org/wikipedia/en/7/7f/The_Five_Heartbeats.jpg", #noqa
         "https://www.youtube.com/watch?v=IKhZXOU5SCg")
 
 movies = [empire_strikes_back, the_godfather, the_five_heartbeats]
